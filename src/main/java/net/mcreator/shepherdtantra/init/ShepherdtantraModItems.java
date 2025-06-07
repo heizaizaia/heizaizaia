b7639474af93fcc5783b18dc2a5c12397e81fe9d
heizaizaizai <cjy121709635@qq.com>
2025-06-07 20:18:52 +0800

    锻造合金

---
+++ src/main/java/net/mcreator/shepherdtantra/init/ShepherdtantraModItems.java
@@ -0,0 +1,19 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.shepherdtantra.init;
+
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.DeferredItem;
+
+import net.minecraft.world.item.Item;
+
+import net.mcreator.shepherdtantra.ShepherdtantraMod;
+
+public class ShepherdtantraModItems {
+	public static final DeferredRegister.Items REGISTRY = DeferredRegister.createItems(ShepherdtantraMod.MODID);
+	public static final DeferredItem<Item> BBLACK_1 = REGISTRY.register("bblack_1", Bblack1Item::new);
+	// Start of user code block custom items
+	// End of user code block custom items
+}
---
+++ src/main/java/net/mcreator/shepherdtantra/item/Bblack1Item.java
@@ -0,0 +1,11 @@
+
+package net.mcreator.shepherdtantra.item;
+
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Item;
+
+public class Bblack1Item extends Item {
+	public Bblack1Item() {
+		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON));
+	}
+}
